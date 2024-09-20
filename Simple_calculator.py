b0466f3891403c4716cdcc38ba0e40d555e3dbf2
ronita00 <162792693+ronita00@users.noreply.github.com>
2024-09-20 11:08:04 +0600

    Create Simple_calculator

---
+++ Simple_calculator.py
@@ -0,0 +1,19 @@
+
+# Python program for simple calculator function
+
+def add(num1, num2):        # Function to add two numbers
+    return num1 + num2
+
+# Function to subtract two numbers
+def subtract(num1, num2):
+    return num1 - num2
+
+# Function to multiply two numbers
+def multiply(num1, num2):
+    return num1 * num2
+
+# Function to divide two numbers
+def divide(num1, num2):
+    return num1 / num2
+
+
