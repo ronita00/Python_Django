0cdc5c5239f82f83a03cef1980f89c5278b91c80
ronita00 <162792693+ronita00@users.noreply.github.com>
2024-11-14 11:26:51 +0600

    ronitaMathModule.py

--- ronitaMathModule.py
+++ ronitaMathModule.py
@@ -2,7 +2,7 @@ from mpmath import mp
 
 # Set precision to 50 decimal places (adjust as needed)
 NoOfDigits=int(input("Enter the number of digits: "))
-mp.dps = noOfDigits
+mp.dps = NoOfDigits
 pi_value = mp.pi
 
 print(f"Value of π : {pi_value}")
